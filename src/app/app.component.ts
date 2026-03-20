9e2225be6c58fa189a7fd96d4479f07f1195d11e
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-20 06:34:12 +0530

    feat: integrate src/app/app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-root',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {
+  constructor(private dataService: DataService) {}
+}
